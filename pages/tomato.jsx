19b42f68740082825662179337af49c856ec5bdc
Junseok <yalvz@naver.com>
2021-02-02 23:26:45 +0900

    데이터를 불러오는 방법
    데이터를 불러오는 방법
    1. getServerSideProps
    2. getStaticProps
    3. getInitialProps

--- pages/tomato.jsx
+++ pages/tomato.jsx
@@ -1,3 +1,14 @@
+/*
+ * Link 컴포넌트
+ * href: string 값으로 이동할 경로 또는 URL입니다.
+ * as: string 값으로 브라우저의 URL에 표시될 값으로, Next 버전 9.5.3 이전에 동적 라우팅을 위하여 사용됩니다.
+ * replace: boolean 값으로 브라우저의 history 스택에 url을 추가하지 않고 현재 상태를 변경합니다.
+ * scroll: boolean 값으로 스크롤을 맨 위로 이동할지 설정하는 값입니다. 기본값을 true입니다.
+ * shallow: boolean 값으로 서버에서 데이터를 불러오는 작업을 스킵할 때 사용합니다. 기본값은 false입니다.
+ * passHref: boolean 값으로 자식에게 href를 전달하게 됩니다. 리액트 컴포넌트에 href를 전달할 수 있게 됩니다.
+ * prefetch: boolean 값으로 백그라운드에서 페이지를 미리 가져오게 됩니다. 
+ *           브라우저 화면의 Link 컴포넌트의 페이지들을 미리 가져오게 되며 기본값은 true입니다.
+ */
 import Link from 'next/link'
 
 const Tomato = () => {
--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -5,22 +5,62 @@ const name = ({user}) => {
     return <div>{username}</div>
 }
 
-export const getServerSideProps = async ({query}) => {
+// getInitialProps: 서버 사이드 데이터 패치를 위해 사용되던 함수
+name.getInitialProps = async ({query}) => {
     const {name} = query
-
     try {
         const res = await fetch(`https://api.github.com/users/${name}`)
-        if(res.status === 200) {
+        if(res.status == 200) {
             const user = await res.json()
-            // 서버사이드 렌더이기 때문에 콘솔에서만 출력이 된다
-            console.log(user)
             return {props: {user}}
         }
-        return {props: {}}
+        return {props: {user}}
     } catch(e) {
         console.log(e)
-        return {props: {}}
+        return {}
     }
 }
+// // getStaticProps : 빌드 시에 데이터를 불러와 결과를 json으로 저장하여 사용. -> 일관된 데이터를 보여주게 된다.
+// export const getStaticProps = async ({params}) => {
+//     try {
+//         const res = await fetch(`https://api.github.com/users/${params.name}`)
+//         const user = await res.json()
+//         if(res.status === 200) {
+//             const user = await res.json()
+//             return {props: {user, time: new Date().toISOString()}}
+//         }
+//         return {props: {time: new Date().toISOString()}}
+//     } catch(e) {
+//         console.log(e)
+//         return {props: {time: new Date().toISOString()}}
+//     }
+// }
+
+// // getStaticPaths: 페이지의 경로가 외부 데이터에 의존할 때 사용된다.
+// export async function getStaticPaths() {
+//     return {
+//         paths: [{params: {name: 'rews7895'}}],
+//         // fallback이 false이면 이외의 경로는 404 error 페이지로 가게 된다.
+//         fallback: true,
+//     }
+// }
+// getServerSideProps : 페이지의 데이터를 서버로부터 제공받는 기능을 제공.
+// export const getServerSideProps = async ({query}) => {
+//     const {name} = query
+
+//     try {
+//         const res = await fetch(`https://api.github.com/users/${name}`)
+//         if(res.status === 200) {
+//             const user = await res.json()
+//             // 서버사이드 렌더이기 때문에 콘솔에서만 출력이 된다
+//             console.log(user)
+//             return {props: {user}}
+//         }
+//         return {props: {}}
+//     } catch(e) {
+//         console.log(e)
+//         return {props: {}}
+//     }
+// }
 
 export default name
